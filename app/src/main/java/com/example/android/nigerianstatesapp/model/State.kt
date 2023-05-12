4b2c95964a32bfb6b29722cfcd2214ea6cf0cc8a
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-12 13:22:05 +0100

    first commit

---
+++ app/src/main/java/com/example/android/nigerianstatesapp/model/State.kt
@@ -0,0 +1,14 @@
+package com.example.android.nigerianstatesapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+
+data class State(
+    val day: Int,
+    @StringRes val stateRes: Int,
+    @StringRes val capitalRes: Int,
+    @StringRes val slogan: Int,
+    @DrawableRes val imageRes: Int,
+    @StringRes val doYouKnowRes : Int,
+    @StringRes val descriptionRes: Int)
---
+++ app/src/main/java/com/example/android/nigerianstatesapp/model/StateRepository.kt
@@ -0,0 +1,10 @@
+package com.example.android.nigerianstatesapp.model
+
+
+
+
+object StateRepository {
+//    val states = listOf(
+//        State(1, "")
+//    )
+}
