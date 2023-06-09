77d48977f394e9c3ad1caf2cc46889482931bc23
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-06-09 15:28:58 +0100

    second commit

--- app/src/main/java/com/example/android/nigerianstatesapp/MainActivity.kt
+++ app/src/main/java/com/example/android/nigerianstatesapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,7 +49,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NigerianStatesApp(modifier: Modifier = Modifier){
-    LazyColumn(modifier = modifier){
+    LazyColumn(modifier = modifier.padding(4.dp)){
         items(states){
             StateInformation(state = it, modifier = Modifier.padding(16.dp))
         }
@@ -53,8 +58,14 @@ fun NigerianStatesApp(modifier: Modifier = Modifier){
 
 @Composable
 fun StateInformation(state: State, modifier: Modifier = Modifier) {
-   Card(modifier = modifier.fillMaxSize(),) {
-       Column {
+   Card(modifier = modifier
+       .fillMaxSize(),
+       shape = RoundedCornerShape(4.dp),
+       elevation = CardDefaults.cardElevation(8.dp)
+       ) {
+       Column(
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.Start ) {
 //           Text(text = stringResource(id = state.day))
            Row() {
                Text(text = stringResource(id = R.string.state))
@@ -72,8 +83,13 @@ fun StateInformation(state: State, modifier: Modifier = Modifier) {
 
 
            
-           Image(painter = painterResource(id = state.imageRes), contentDescription = null,
-           modifier = Modifier.fillMaxWidth())
+           Image(painter = painterResource(id = state.imageRes), contentDescription = stringResource(
+               id = R.string.state
+           ),
+           modifier = Modifier
+               .fillMaxSize()
+               .padding(8.dp),
+           contentScale = ContentScale.Crop)
        }
 
    }
